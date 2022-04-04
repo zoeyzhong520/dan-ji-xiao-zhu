3f16ece4f8415366667b72b848ef98da63323b39
Joe <787589291@qq.com>
2022-04-04 17:21:44 +0800

    完善首页布局；

--- src/pages/components/djxzHeader/djxzHeader.js
+++ src/pages/components/djxzHeader/djxzHeader.js
@@ -1,21 +1,20 @@
 import './djxzHeader.scss'
 import { gameTypesList } from '../../common/common'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 const DJXZHeader = () => {
 
-    const [isGameType, setGameType] = useState(true)    
-
-    const searchTabs = [
-        { title: '角色扮演', type: 'C' },
-        { title: '即时战略', type: 'I' },
-        { title: '体育竞技', type: 'Q' },
-        { title: '休闲益智', type: 'L' },
-        { title: '射击游戏', type: 'B' },
-        { title: '动作游戏', type: 'A' },
-        { title: '赛车竞速', type: 'G' },
-        { title: '动作冒险', type: 'D' }
-    ]
+    const [isGameType, setGameType] = useState(true)
+    const [isQRCode, setQRCode] = useState(false)
+    const [typesList, setTypesList] = useState([])
+    useEffect(() => {
+        // 数组尾部追加一个 更多
+        let list = gameTypesList.slice(1, 10)
+        list.push({
+            title: '更多类型', type: ''
+        })
+        setTypesList(list)
+    }, [])
 
     return (
         <div className='djxzHeader_page'>
@@ -24,7 +23,20 @@ const DJXZHeader = () => {
                 <div className="l"><a href='#'>请登录</a></div>
                 <div className="r">
                     <ul>
-                        <li><a href='#'>小程序码</a></li>
+                        <li>
+                            <a href='#'>
+                                小程序码
+                                {/* 小程序码 */}
+                                <div className="qrCode">
+                                    <div className="contain">
+                                        {/* 小三角 */}
+                                        <div className="triangle"></div>
+                                        <img src={require('../../common/static/mpCode.jpg')} alt="" />
+                                        <p>查看小程序</p>
+                                    </div>
+                                </div>
+                            </a>
+                        </li>
                         <li><a href='#'>帮助中心</a></li>
                         <li><a href='#'>建议反馈</a></li>
                     </ul>
@@ -34,13 +46,12 @@ const DJXZHeader = () => {
             {/* logo、搜索框 */}
             <div className="middle">
                 <div className='logoTitle'>
-                    <img src="" alt="logo" />
-                    <h3>DJXZ</h3>
+                    <img src={require('./static/logo_pc.png')} alt="logo" />
                 </div>
                 <div className='search'>
                     <input type="text" placeholder='搜索您想要的游戏 如 侠盗飞车 或 仙剑奇侠传' />
                     <ul>
-                        {searchTabs.map((item, index) => {
+                        {gameTypesList.slice(3, 11).map((item, index) => {
                             return <li key={index}><a href='#'>{item.title}</a></li>
                         })}
                     </ul>
@@ -56,16 +67,31 @@ const DJXZHeader = () => {
                 </ul>
             </div>
 
-            {/* 游戏分类 */}
+            {/* 全部游戏分类 */}
             <div className="game_type">
                 <div className="game_type_child">
-                    <p onClick={() => { setGameType(!isGameType) }}>游戏分类</p>
+                    <p onClick={() => { setGameType(!isGameType) }}>全部游戏分类</p>
                     <div
                         style={{ display: !isGameType ? 'none' : 'block' }}
                         className="game_type_child_list">
                         <ul>
-                            {gameTypesList.map((item, index) => {
-                                return <li key={item.type}><a href='#'>{item.title}</a></li>
+                            {typesList.map((item, index) => {
+                                return <li key={item.type}>
+                                    <a href='#'>
+                                        {item.title}
+
+                                        {/* 更多游戏类型 */}
+                                        <div className="more">
+                                            <ul>
+                                                {gameTypesList.slice(11, 19).map((item, index) => {
+                                                    return <li key={item.title}>
+                                                        <a href='#'>{item.title}</a>
+                                                    </li>
+                                                })}
+                                            </ul>
+                                        </div>
+                                    </a>
+                                </li>
                             })}
                         </ul>
                     </div>
--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -2,8 +2,19 @@ import './home.scss'
 import DJXZHeader from '../components/djxzHeader/djxzHeader'
 import DJXZFooter from '../components/djxzFooter/djxzFooter'
 import { gameTypesList } from '../common/common'
+import { useState } from 'react'
 
 const Home = () => {
+
+    const [informationAnimate, setInformationAnimate] = useState(false)
+
+    // 列表表头
+    const mainListTitles = [
+        { title: '推荐游戏', icon: './static/Thumb-Up.png' },
+        { title: '人气游戏', icon: './static/Heart.png' },
+        { title: '最新游戏', icon: './static/Hourglass.png' }
+    ]
+
     return (
         <div className='home_page'>
             {/* 头部 */}
@@ -13,60 +24,94 @@ const Home = () => {
             <div className="main">
                 {/* 广告位 */}
                 <div className="ad">
-                    <img src={require('./static/manik-rathee-a8YV2C3yBMk-unsplash.jpg')} alt="" />
+                    <img src={require('./static/BingWallpaper-2020-07-28.jpg')} alt="" />
+                    {/* 小程序码 */}
+                    <div className="mpCode">
+                        <img src={require('../common/static/mpCode.jpg')} alt="" />
+                        <p>扫一扫查看小程序<br/>把服务带在身边</p>
+                    </div>
                 </div>
 
-                {/* 推荐游戏 */}
+                {/* 推荐游戏 人气游戏 最新游戏 */}
+                <div>
+                    {mainListTitles.map((item, index) => {
+                        return <div className="recommend" key={item.title}>
+                            <div className="recommend_title">
+                                <div className='l'>
+                                    <img src={require(`${item.icon}`)} alt="" />
+                                    <p>{item.title}</p>
+                                </div>
+                                <a href='#'>更多{'>>'}</a>
+                            </div>
+                            <div className="recommend_main">
+                                <ul style={{ width: gameTypesList.length * 390 }}>
+                                    {gameTypesList.map((item, index) => {
+                                        return <li key={item.title}>
+                                            <img src="https://media.st.dl.pinyuncloud.com/steam/apps/428660/header.jpg?t=1604053729" alt="图片" />
+                                            <p>飞向月球/Deliver Us The Moon 飞向月球/Deliver Us The Moon{index + 1}</p>
+                                        </li>
+                                    })}
+                                </ul>
+                            </div>
+                        </div>
+                    })}
+                </div>
+
+                {/* 想要游戏 */}
+                <div className="want">
+                    <div className="child">
+                        <div className="l">
+                            <p className='en'>Want Game</p>
+                            <p>想要游戏</p>
+                        </div>
+                        <div className="r">
+                            <div>了解更多</div>
+                        </div>
+                    </div>
+                </div>
+
+                {/* 热评资讯 */}
                 <div className="recommend">
                     <div className="recommend_title">
-                        <p>推荐游戏</p>
+                        <div className='l'>
+                            <img src={require('./static/Book-Open.png')} alt="" />
+                            <p>热评资讯</p>
+                        </div>
                         <a href='#'>更多{'>>'}</a>
                     </div>
-                    <div className="recommend_main">
-                        <ul style={{width: gameTypesList.length*390}}>
-                            {gameTypesList.map((item, index) => {
-                                return <li>
-                                    <img src="https://media.st.dl.pinyuncloud.com/steam/apps/428660/header.jpg?t=1604053729" alt="图片" />
-                                    <p>飞向月球/Deliver Us The Moon 飞向月球/Deliver Us The Moon{index+1}</p> 
+                    <div className="information_main">
+                        <div
+                            onClick={() => { setInformationAnimate(true) }}
+                            className="information_main_before">
+                                <img src={require('../common/static/Arrow-Signle-Left.png')} alt="" />
+                            </div>
+                        <div
+                            onClick={() => { setInformationAnimate(false) }}
+                            className="information_main_after">
+                                <img src={require('../common/static/Arrow-Signle-Right.png')} alt="" />
+                            </div>
+                        <ul style={{ animation: informationAnimate ? 'move 1s forwards' : 'moveBack 1s forwards' }}>
+                            {['qaz', 'wsx', 'edc', 'rfv'].map((item, index) => {
+                                return <li key={item}>
+                                    <img src='http://img4.yxdimg.com/2021/4/27/452c1710-856f-4541-9aee-61f7c66ab263.jpg' alt=''></img>
+                                    <p>《永劫无间》内测版评测：解决了冷兵器吃鸡的核心痛点</p>
+                                    <p>《永劫无间》是一款吸纳了诸多武侠吃鸡游戏失败经验的作品，它通过飞索提供的超高机动性，很好地解决了玩家交战意愿低的问题。同时，出色的格斗机制，也让它显得颇具可玩性。</p>
                                 </li>
                             })}
                         </ul>
                     </div>
                 </div>
 
-                {/* 人气游戏 */}
-                <div className="recommend">
-                    <div className="recommend_title">
-                        <p>人气游戏</p>
-                        <a href='#'>更多{'>>'}</a>
-                    </div>
-                    <div className="recommend_main">
-                        <ul style={{width: gameTypesList.length*390}}>
-                            {gameTypesList.map((item, index) => {
-                                return <li>
-                                    <img src="https://media.st.dl.pinyuncloud.com/steam/apps/428660/header.jpg?t=1604053729" alt="图片" />
-                                    <p>飞向月球/Deliver Us The Moon 飞向月球/Deliver Us The Moon{index+1}</p> 
-                                </li>
-                            })}
-                        </ul>
-                    </div>
-                </div>
-
-                {/* 最新游戏 */}
-                <div className="recommend">
-                    <div className="recommend_title">
-                        <p>最新游戏</p>
-                        <a href='#'>更多{'>>'}</a>
-                    </div>
-                    <div className="recommend_main">
-                        <ul style={{width: gameTypesList.length*390}}>
-                            {gameTypesList.map((item, index) => {
-                                return <li>
-                                    <img src="https://media.st.dl.pinyuncloud.com/steam/apps/428660/header.jpg?t=1604053729" alt="图片" />
-                                    <p>飞向月球/Deliver Us The Moon 飞向月球/Deliver Us The Moon{index+1}</p> 
-                                </li>
-                            })}
-                        </ul>
+                {/* 想要游戏 */}
+                <div className="want" style={{ height: '140px' }}>
+                    <div className="child">
+                        <div className="l">
+                            <p className='en' style={{ top: '10px' }}>Want Game</p>
+                            <p>想要游戏</p>
+                        </div>
+                        <div className="r">
+                            <div>了解更多</div>
+                        </div>
                     </div>
                 </div>
             </div>
